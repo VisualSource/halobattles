703685c317c27e52a595f4d17d9a202ad8fd0c12
VisualSource <collin_blosser@yahoo.com>
2023-08-22 16:18:23 -0400

    update editor

--- server/src/map/upgradeList.ts
+++ server/src/map/upgradeList.ts
@@ -1,4 +1,5 @@
 import type { AttackType, EffectiveState, UnitType } from "./units.js";
+import ItemJson from '../../meta/buildings.json' assert { type: "json" };
 
 export type BuildingStat = {
     stat: "cap.current" | "cap.max" | "credits.income" | "nostat"
@@ -28,7 +29,6 @@ interface Buildable {
         }
     }
 }
-
 export interface BuildingData extends Buildable {
     type: "building";
     battle: null | {
@@ -43,124 +43,8 @@ export interface BuildingData extends Buildable {
         },
     }
 }
-
 export interface TechData extends Buildable {
     type: "tech";
 }
-
-
-export const buildOptions = new Map<number, BuildingData | TechData>([
-    [0, {
-        id: 0,
-        icon: "https://halo.wiki.gallery/images/b/b0/HW_FieldArmory_Concept.jpg",
-        name: "Field Armory",
-        type: "building",
-        description: "The UNSC's Field Armory is a cross between a machine\fabrication shop, and a research laboratory. This is where the most advanced technology for the UNSC is created by dedicated engineers and scientists.",
-        maxLevel: 5,
-        requires: [],
-        max: {
-            global: -1,
-            node: 1
-        },
-        battle: {
-            effective: {
-                "air": "normal",
-                "building": "normal",
-                "infantry": "normal",
-                "vehicle": "normal"
-            },
-            type: "building",
-            health: 100,
-            shealds: 0,
-            attack: 0,
-            hitChange: 0,
-            damageType: "kinetic",
-        },
-        levels: {
-            1: {
-                values: [{
-                    value: 200,
-                    stat: "cap.max",
-                    color: "green",
-                    text: "+200 Unit Cap",
-                }],
-                build: {
-                    time: 10,
-                    cost: 20_000,
-                }
-            },
-            2: {
-                values: [
-                    {
-                        value: 210,
-                        stat: "cap.max",
-                        color: "green",
-                        text: "+210 Unit Cap",
-                    }
-                ],
-                build: {
-                    time: 10,
-                    cost: 30_000
-                }
-            }
-        }
-    }],
-    [
-        1,
-        {
-            id: 1,
-            icon: "https://halo.wiki.gallery/images/thumb/5/5e/HW2_Blitz_Bunker_Drop.jpg/1600px-HW2_Blitz_Bunker_Drop.jpg",
-            name: "Citadel",
-            type: "building",
-            description: "Defend this with your life. lose this and its all over.",
-            maxLevel: 1,
-            requires: [],
-            max: {
-                global: 1,
-                node: 1
-            },
-            battle: {
-                effective: {
-                    "air": "normal",
-                    "building": "weak",
-                    "infantry": "normal",
-                    "vehicle": "normal"
-                },
-                type: "building",
-                health: 500,
-                shealds: 0,
-                attack: 120,
-                hitChange: 60,
-                damageType: "kinetic",
-            },
-            levels: {
-                1: {
-                    values: [
-                        {
-                            value: 200,
-                            stat: "cap.max",
-                            color: "green",
-                            text: "+200 Unit Cap",
-                        },
-                        {
-                            value: 1_000,
-                            stat: "credits.income",
-                            color: "green",
-                            text: "+1,000 Income",
-                        },
-                        {
-                            value: 0,
-                            stat: "event",
-                            run: "destory",
-                            color: "red",
-                            text: "Lose on destory",
-                            event: "player-lose-objective"
-                        }
-                    ],
-                    build: null
-                }
-            }
-        }
-    ]
-]);
-
+export type Item = BuildingData | TechData;
+export const buildOptions = new Map<number, Item>(ItemJson as [number, Item][]);
