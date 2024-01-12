6ead114d2036cad0ea0696a6cab68e906404ea7e
VisualSource <collin_blosser@yahoo.com>
2024-01-12 17:57:05 -0500

    Mark as todo

--- server/src/lib/procedures/getBuildOptions.ts
+++ server/src/lib/procedures/getBuildOptions.ts
@@ -3,12 +3,14 @@ import { z } from "zod";
 
 const getBuildOptions = procedure.input(z.string().uuid()).query(({ ctx, input }) => {
 
+    // TODO
+
     // fetch planet get buildings
     // get player global tech.
 
     // return all options that are vaild to this planet
 
-    return [{ id: "", icon: "", }];
+    return [];
 });
 
 export default getBuildOptions;
